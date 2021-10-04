5ef37813524038d4f92b55c5911274cd39be403e
Abdielol <abdieldluna@gmail.com>
2021-10-04 20:53:16 +0000

    uppercase and lowercase alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - alphabet in lowercase and uppercase
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int l = 97;
+
+int u = 65;
+
+int y = 10;
+
+while (l <= 122)
+{
+putchar(l);
+l++;
+}
+putchar (y);
+return (0);
+}
