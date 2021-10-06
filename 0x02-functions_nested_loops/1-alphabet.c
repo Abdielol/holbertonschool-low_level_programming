b3bc3e2a4d7161e3b7cef034918065ff82fab49a
Abdielol <abdieldluna@gmail.com>
2021-10-06 16:55:44 +0000

    Add: putchar function and alpahabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - print the alphabet
+ *
+ * Return: 0
+ */
+int print_alphabet(void)
+{
+int a = 97;
+while (a <= 122)
+{
+_putchar(a);
+a = a + 1;
+}
+_putchar('\n');
+
+return (0);
+
+} 
