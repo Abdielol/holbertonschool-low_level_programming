86073b3e4d6c24c70f6748f86c39fc2d0284bf3b
Abdielol <abdieldluna@gmail.com>
2021-10-07 15:28:44 +0000

    printing numbers again

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - number with one digit
+ *
+ * Return: 0
+ */
+int main(void)
+{
+int c = 48;
+while (c <= 57)
+{
+putchar(c);
+c++;
+}
+putchar(10);
+
+return (0);
+}
