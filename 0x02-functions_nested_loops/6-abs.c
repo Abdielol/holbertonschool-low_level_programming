85a7e81379b637c275e952a650b9182d54f4a24b
Abdielol <abdieldluna@gmail.com>
2021-10-07 19:15:02 +0000

    add abs and new prototype

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,27 +1,15 @@
 #include "main.h"
 /**
- * print_sign - print the sing
- * @n: sing
- * Return: c
- **/
-int print_sign(int n)
+ * _abs - absolute value of an integer.
+ * @i: is an integer
+ * Return: 0;
+ */
+int _abs(int i)
 {
-if (n >= 1)
+if (i < 0)
 {
-_putchar(43);
-n = 1;
-}
-else if (n <= -1)
-{
-_putchar(45);
-n = -1;
-}
-else
-{
-_putchar(48);
-n = 0;
+i = i * -1;
 }
 
-return (n);
-
+return (i);
 }
