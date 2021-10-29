1161acbf7c79c1a93d134c259adc7bd2adab8d0d
Abdielol <abdieldluna@gmail.com>
2021-10-29 15:16:24 +0000

    how to copy with memory allocated and concanating two strings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "main.h"
+/**
+ * _strdup - allocated space in memory
+ * @str: char
+ * Return: pointer
+ */
+char *_strdup(char *str)
+{
+int acum = 0;
+int x = 0;
+char *ma;
+
+if (str == NULL)
+{
+return (NULL);
+}
+
+while (str[x] != '\0')
+{
+acum += 1;
+x++;
+}
+
+ma = malloc(sizeof(char) * (acum + 1));
+
+if (!ma)
+{
+return (NULL);
+}
+
+x = 0;
+
+while (str[x] != '\0')
+{
+ma[x] = str[x];
+x++;
+}
+
+ma[x] = '\0';
+
+return (ma);
+
+}
